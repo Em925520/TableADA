0258c9e9ae6f2d80fb4f9f7b42d42e7e92ffb168
Emily Xie <xiee178@newschool.edu>
2023-01-15 03:35:38 -0500

    audio on food asset works: when your hand touches the spagetti, it says HEYYY!

--- Assets/scripts/Say_Hey.cs
+++ Assets/scripts/Say_Hey.cs
@@ -7,7 +7,7 @@ public class Say_Hey : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip clip;
     public float volume = 0.5f;
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
         audioSource.PlayOneShot(clip, volume);
     }
